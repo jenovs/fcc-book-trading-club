8a73b0b64c6356c7ab89d80a84ede8341529d07a
Viktors Jenovs <jviktors@gmail.com>
2017-02-20 19:27:19 +0200

    Fix authentication bug

--- app/routes/books.js
+++ app/routes/books.js
@@ -12,6 +12,7 @@ if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development') {
   checkAuth = (req, res, next) => {
     console.log('checking auth...', req.user);
     if (!req.user) return res.status(401).send();
+    next();
   }
 }
 
