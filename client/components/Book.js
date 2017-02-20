6b669386a288c0d325f6311b6f547d6f6e4e1f7e
Viktors Jenovs <jviktors@gmail.com>
2017-02-20 12:28:50 +0200

    Remove hover

--- client/components/Book.js
+++ client/components/Book.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classNames from 'classnames';
+// import classNames from 'classnames';
 
 import BtnDelete from './BtnDelete';
 import BtnRequest from './BtnRequest';
@@ -7,39 +7,8 @@ import Button from './Button';
 
 export default class Book extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      hovered: false
-    }
-    this.handleHover = this.handleHover.bind(this);
-    this.handleUnhover = this.handleUnhover.bind(this);
-    this.toggleHover = this.toggleHover.bind(this);
-  }
-
-  handleHover() {
-    this.setState({
-      hovered: true
-    })
-  }
-  handleUnhover() {
-    this.setState({
-      hovered: false
-    })
-  }
-
-  toggleHover() {
-    this.setState({
-      hovered: !this.state.hovered
-    })
-  }
-
   render() {
     // console.log('Book props', this.props);
-    const bookClass = classNames({
-      'book': true,
-      'book--hovered': this.state.hovered
-    });
     const cover = {
       backgroundImage: `url(${this.props.book.coverUrl})`,
       backgroundSize: 'cover'
@@ -48,22 +17,20 @@ export default class Book extends React.Component {
     // console.log('Book, user', user);
     return (
       <div
-        className={bookClass}
+        className="book"
         title={`${this.props.book.author}\n${this.props.book.title}`}
         style={cover}
-        onMouseEnter={this.handleHover}
-        onMouseLeave={this.handleUnhover}
-        onTouchStart={this.toggleHover}>
+      >
         {!this.props.book.coverUrl && this.props.book.author}<br/>
         {!this.props.book.coverUrl && this.props.book.title}
-        {!book._requestedBy && <BtnRequest {...this.props} hovered={this.state.hovered} />}
-        {this.props.showDelete && <BtnDelete {...this.props} hovered={this.state.hovered} />}
+        {!book._requestedBy && <BtnRequest {...this.props} hovered={true} />}
+        {this.props.showDelete && <BtnDelete {...this.props} hovered={true} />}
         {this.props.requests &&
           <Button
             caption={'Cancel'}
             color={'red'}
             top={5}
-            show={this.state.hovered}
+            show={true}
             handleClick={this.props.deleteTradeRequest}
           />}
         {(this.props.requests && user._id !== book._requestedBy) &&
@@ -71,7 +38,7 @@ export default class Book extends React.Component {
             caption={'Confirm'}
             color={'cornflowerblue'}
             top={30}
-            show={this.state.hovered}
+            show={true}
             handleClick={this.props.confirmTradeRequest}
           />}
       </div>
