c00eecba57055b7666bdfe01502651272f0eb9b7
Viktors Jenovs <jviktors@gmail.com>
2017-02-21 11:50:43 +0200

    Add styling

--- client/components/Book.js
+++ client/components/Book.js
@@ -16,31 +16,37 @@ export default class Book extends React.Component {
     const { book, user } = this.props;
     // console.log('Book, user', user);
     return (
-      <div
-        className="book"
-        title={`${this.props.book.author}\n${this.props.book.title}`}
-        style={cover}
-      >
-        {!this.props.book.coverUrl && this.props.book.author}<br/>
-        {!this.props.book.coverUrl && this.props.book.title}
-        {!book._requestedBy && <BtnRequest {...this.props} hovered={true} />}
-        {this.props.showDelete && <BtnDelete {...this.props} hovered={true} />}
-        {this.props.requests &&
-          <Button
-            caption={'Cancel'}
-            color={'red'}
-            top={5}
-            show={true}
-            handleClick={this.props.deleteTradeRequest}
-          />}
-        {(this.props.requests && user._id !== book._requestedBy) &&
-          <Button
-            caption={'Confirm'}
-            color={'cornflowerblue'}
-            top={30}
-            show={true}
-            handleClick={this.props.confirmTradeRequest}
-          />}
+      <div className="book__container">
+        <div
+          className="book__cover"
+          title={`${this.props.book.author}\n${this.props.book.title}`}
+          style={cover}
+        >
+          {!this.props.book.coverUrl && this.props.book.author}<br/>
+          {!this.props.book.coverUrl && this.props.book.title}
+          {!book._requestedBy && <BtnRequest {...this.props} hovered={true} />}
+          {this.props.showDelete && <BtnDelete {...this.props} hovered={true} />}
+          {this.props.requests &&
+            <Button
+              caption={'Cancel'}
+              color={'red'}
+              top={5}
+              show={true}
+              handleClick={this.props.deleteTradeRequest}
+            />}
+          {(this.props.requests && user._id !== book._requestedBy) &&
+            <Button
+              caption={'Confirm'}
+              color={'cornflowerblue'}
+              top={30}
+              show={true}
+              handleClick={this.props.confirmTradeRequest}
+            />}
+        </div>
+        {/* <div className="book__footer">
+          {this.props.book.title}<br/>
+          by {this.props.book.author}
+        </div> */}
       </div>
     )
   }
